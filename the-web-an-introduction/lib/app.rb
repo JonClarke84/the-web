e5e1a88cf1ea83133511cd1254f048266c79ab53
Jon Clarke <jonclarke1984@googlemail.com>
2022-03-21 16:16:14 +0000

    fun with params

--- the-web-an-introduction/lib/app.rb
+++ the-web-an-introduction/lib/app.rb
@@ -2,18 +2,27 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  "hello world"
+  'hello world'
 end
 
 get '/bye' do
-  "goodbye cruel world"
+  'goodbye cruel world'
 end
 
 get '/secret' do
-  "<h1>this is a very secret page</h1>"
+  '<h1>this is a very secret page</h1>'
 end
 
-get '/cat' do
-  @random_name = ["Amigo", "Misty", "Almond"].sample
+get '/random-cat' do
+  @name = %w[Amigo Misty Almond].sample
+  @day = Time.now.strftime('%A')
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  @age = params[:age]
+  @day = Time.now.strftime('%A')
   erb(:index)
 end
